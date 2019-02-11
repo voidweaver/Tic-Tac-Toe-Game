54477b2c23c25f0cc503f8e6688e4e32bee5cd96
Omsin Krissada <46844213+OmsinKrissada@users.noreply.github.com>
2019-02-11 18:02:28 +0700

    Update TicTacToe.h

--- Tic Tac Toe/src/TicTacToe.h
+++ Tic Tac Toe/src/TicTacToe.h
@@ -1,19 +1,28 @@
 #pragma once
 
+bool inlimit(int since, int until, int input);
+
 class TicTacToe {
 
 private:
 	int round;
+	int board_size;
+	char* tile;
 	std::string winner;
 	std::string player1_name;
 	std::string player2_name;
-	int board_size;
-	char* board;
+	
 
 public:
 	TicTacToe(int board_size);
 	~TicTacToe();
+	
+	void setPlayer1_name(std::string name);
+	void setPlayer2_name(std::string name);
+	
 	void start();
+	void checkwin();
+	void gameover();
 
 
 };
